624e518ce6a69b408f1555125599b342eea059e0
RealSanjeev <45820805+realsanjeev@users.noreply.github.com>
2022-12-13 15:16:55 +0000

    doing action

--- app.py
+++ app.py
@@ -10,17 +10,22 @@ Session(app)
 
 @app.route('/')
 def initiate():
-    return redirect('/login')
+    return render_template('login.html')
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('index.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        with open('user.txt') as f:
+            f.write(username)
+    return render_template('signin.html')
 
 @app.route('/signup')
 def signup():
-    return 'From sign up'
+    return render_template('signup.html')
 
-@app.route('/logout')
+@app.route('/logout', methods=["GET", "POST"])
 def logout():
     return redirect('/login')
 if __name__=="__main__":
